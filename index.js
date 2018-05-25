4d9bfa7b696bb2fe0a3c0e606c92e65cb681841b
Denise Pendleton <dpendl1@hotmail.com>
2018-05-25 18:52:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ function appendDriver(name){
 }
 
 function prependDriver(name){
-const NewDrivers = [name, ...drivers]
+const NewDrivers = [name, ...drivers];
+return newDrivers;
 }
 
 function removeLastDriver(){
