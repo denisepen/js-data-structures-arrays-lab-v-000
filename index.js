793a4f19b72be686fa183ef8baa65c965a9ef2b7
Denise Pendleton <dpendl1@hotmail.com>
2018-05-25 18:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your solution here!
-const drivers = ["Milo", "Otis", "Garfield"]
+const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name){
-  drivers.push(name)
+  drivers.push(name);
 }
 
 function destructivelyPrependDriver(name){
-  drivers.unshift(name)
+  drivers.unshift(name);
 }
